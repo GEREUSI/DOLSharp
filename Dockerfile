1c0af11767e963a9468eb2b0809b562cf56b5488
Alex Prismont <aprishmont@gmail.com>
2021-04-09 13:30:51 +0300

    Fix deployment pipeline issues

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM mono:6.10
 COPY . /app
 RUN /app/build.sh
-CMD [ "run.sh" ]
-
-
+CMD [ "/app/run.sh" ]
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 mkdir -p /app/Release/config
-sed 's/{{password}}/'"MYSQL_PASSWORD"'/g;s/{{db}}/'"MYSQL_DATABASE"'/g;s/{{user}}/'"MYSQL_USER"'/g' serverconfig.xml > /app/Release/config/serverconfig.xml
+sed "s/{{password}}/${MYSQL_PASSWORD}/g;s/{{db}}/${MYSQL_DATABASE}/g;s/{{user}}/${MYSQL_USER}/g" /app/serverconfig.xml > /app/Release/config/serverconfig.xml
+
 mono /app/Release/DOLServer.exe
